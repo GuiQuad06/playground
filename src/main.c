9a81c15c082f5092f5c4e752035b9751531629cf
GuiQuad06 <guillaume.cuadrado@gmail.com>
2023-12-03 23:35:54 +0100

    Backbone for file-handling.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,38 @@
-#ifndef __NO_SYSTEM_INIT
-void SystemInit()
-{}
-#endif
+#include <stdlib.h>
+#include <stdio.h>
 
-void main()
+#define NMAX    10000
+
+int main()
 {
-	for (;;);
+    FILE *fptr;
+
+    // Open a file in read mode
+    fptr = fopen("../input.txt", "r");
+
+    // Store the content of the file
+    char myString[NMAX];
+
+    // If the file exist
+    if(fptr != NULL)
+    {
+        // Read the content and print it
+        // "for line in text:" equivalent
+        while(fgets(myString, NMAX, fptr))
+        {
+            // Do the thing
+        }
+
+    }
+    // If the file does not exist
+    else
+    {
+
+        printf("Not able to open the file.");
+    }
+
+    // Close the file
+    fclose(fptr);
+
+	return 0;
 }
