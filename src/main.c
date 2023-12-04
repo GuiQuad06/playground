60a7d1e6fded43d8593133015e16f182d50d2818
GuiQuad06 <guillaume.cuadrado@gmail.com>
2023-12-04 14:33:35 +0100

    Do the puzzle.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define NMAX    10000
+
+static bool isNumber(const char ch)
+{
+    return ((ch >= '0' && ch <= '9') ? true : false);
+}
 
 int main()
 {
     FILE *fptr;
+    unsigned int first, last, indx, acc;
+    unsigned int cpt_lines = 0;
 
     // Open a file in read mode
     fptr = fopen("../input.txt", "r");
@@ -20,9 +29,36 @@ int main()
         // "for line in text:" equivalent
         while(fgets(myString, NMAX, fptr))
         {
-            // Do the thing
-        }
+            unsigned char size = strlen(myString) - 2;
 
+            // Find first number 
+            indx = 0;
+            while(indx <= size)
+            {
+                if(isNumber(myString[indx]))
+                {
+                    char tmp[2] = { myString[indx], '\0'}; 
+                    first = atoi(tmp);
+                    break;
+                }
+                indx++;
+            }
+            // Find last number
+            indx = size;
+            while(indx >= 0)
+            {
+                if(isNumber(myString[indx]))
+                {
+                    char tmp[2] = { myString[indx], '\0'}; 
+                    last = atoi(tmp);
+                    break;
+                }
+                indx--;
+            }
+            acc += (10 * first + last); 
+            cpt_lines++;
+            printf("acc=%u\n",(10*first+last));
+        }
     }
     // If the file does not exist
     else
@@ -33,6 +69,8 @@ int main()
 
     // Close the file
     fclose(fptr);
+
+    printf("Result: %u\nNb processed lines:%u\n", acc, cpt_lines);
 
 	return 0;
 }
