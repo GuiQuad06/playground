651b047c2da8db702ba5857ecb6826be65c2833c
GuiQuad06 <guillaume.cuadrado@gmail.com>
2023-12-28 22:50:56 +0100

    Puzzle resolution

--- src/main.c
+++ src/main.c
@@ -2,8 +2,57 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define NMAX    10000
+#define NMAX      10000
+#define ASCII_SIZE    7
+
+static void process_char(char ch)
+{
+    bool isFirstBit = true;
+    char old, tmp;
+    char data[ASCII_SIZE-1];
+    unsigned int occ[ASCII_SIZE-1];
+    unsigned int k = 0;
+
+    for(int i = ASCII_SIZE-1 ; i >= 0 ; i--)
+    {
+        tmp = (ch >> i) & 0x01;
+
+        if(isFirstBit)
+        {
+            data[k] = tmp;
+            occ[k] = 1;
+            isFirstBit = false;
+        }
+        else if(tmp != old)
+        {
+            data[++k] = tmp;
+            occ[k] = 1;
+        }
+        else 
+        {
+            occ[k]++;
+        }
+        old = tmp;
+    }
+    // data contient le first block
+    // occ contient le second block
+    // k est le nombre de suite (sert a sizer les deux blocks)
+}
+
+static void process(const char * str)
+{
+    const char * ptr;
+    ptr = str;
+
+    while(*ptr != '\0')
+    {
+        // Passe le caractere
+        process_char(*ptr);
+        ptr++;
+    }
+}
 
 int main()
 {
@@ -35,16 +84,8 @@ int main()
     // Close the file
     fclose(fptr);
 
-    // Exemple de parcours de la chaine extraite :
-    char * ptr = myString;
-    while(*ptr != '\0')
-    {
-        *ptr = toupper(*ptr);
-        ptr++;
-    }
-
-    printf("%s\n", myString);
-    // Fin de l'exemple
+    // Passe la chaine
+    process(myString);
 
 	return 0;
 }
