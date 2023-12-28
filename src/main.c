5e55159be643494fade27f6e9f25855ec5c01555
GuiQuad06 <guillaume.cuadrado@gmail.com>
2023-12-28 22:50:56 +0100

    Puzzle resolution

--- src/main.c
+++ src/main.c
@@ -1,9 +1,72 @@
-#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define NMAX    10000
+#define ASCII_SIZE                   (7u)
+#define NMAX                       (101u)
+
+static void process(const char * str)
+{
+    unsigned int k = 0, cnt = 0; 
+    char old, tmp;
+    bool isFirstBit = true;
+	
+    unsigned int max_size = strlen(str) * ASCII_SIZE * 4; 
+    char *res = malloc(max_size * sizeof(char));
+	
+    size_t pos = 0;
+    const char * ptr;
+    ptr = str;
+
+    // Je parcours les caracteres
+    while(*ptr != '\0')
+    {
+        // Je parcours les bits du caractere
+        for(int i = ASCII_SIZE-1 ; i >= 0 ; i--)
+        {
+            tmp = (*ptr >> i) & 0x01;
+
+            if(isFirstBit)
+            {
+                if(tmp == 1)
+                    pos += snprintf(res+pos, max_size-pos, "0 ");
+                else if(tmp == 0)
+                    pos += snprintf(res+pos, max_size-pos, "00 ");
+                else {}
+                cnt = 1;
+                isFirstBit = false;
+            }
+            else if(tmp != old)
+            {
+                for(int j = 0; j < cnt; j++)
+                {
+                    pos += snprintf(res+pos, max_size-pos, "0");
+                }
+                pos += snprintf(res+pos, max_size-pos, " ");
+                
+                if(tmp == 1)
+                    pos += snprintf(res+pos, max_size-pos, "0 ");
+                else if(tmp == 0)
+                    pos += snprintf(res+pos, max_size-pos, "00 ");
+                else {}
+                
+                cnt = 1;
+            }
+            else 
+            {
+                cnt++;
+            }
+            old = tmp;
+        }
+        ptr++;
+    }
+    for (int j = 0; j < cnt; j++)
+        pos += snprintf(res+pos, max_size-pos, "0");
+
+    printf("%s", res);
+    free(res);
+}
 
 int main()
 {
@@ -35,16 +98,8 @@ int main()
     // Close the file
     fclose(fptr);
 
-    // Exemple de parcours de la chaine extraite :
-    char * ptr = myString;
-    while(*ptr != '\0')
-    {
-        *ptr = toupper(*ptr);
-        ptr++;
-    }
-
-    printf("%s\n", myString);
-    // Fin de l'exemple
+    // Passe la chaine
+    process(myString);
 
 	return 0;
 }
