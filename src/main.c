fd38cb430b5536729a17a371d5141c139b121872
Guillaume CUADRADO <gucd@oticonmedical.com>
2024-09-03 16:40:44 +0200

    Functionnal test of the crc function

--- src/main.c
+++ src/main.c
@@ -3,48 +3,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NMAX    10000
+#include "crc.h"
 
 int main()
 {
-    FILE *fptr;
-
-    // Open a file in read mode
-    fptr = fopen("../input.txt", "r");
-
-    // Store the content of the file
-    char myString[NMAX];
-
-    // If the file exist
-    if(fptr != NULL)
-    {
-        // Read the content and print it
-        // "for line in text:" equivalent
-        while(fgets(myString, NMAX, fptr))
-        {
-            // Do the thing
-        }
-
-    }
-    // If the file does not exist
-    else
-    {
-        printf("Not able to open the file.");
-    }
-
-    // Close the file
-    fclose(fptr);
-
-    // Exemple de parcours de la chaine extraite :
-    char * ptr = myString;
-    while(*ptr != '\0')
-    {
-        *ptr = toupper(*ptr);
-        ptr++;
-    }
-
-    printf("%s\n", myString);
-    // Fin de l'exemple
-
+    const char* toto = "A";
+    printf("Le CRC16 de A est: 0x%4x\n", crc16_ccitt(toto));
     return 0;
 }
